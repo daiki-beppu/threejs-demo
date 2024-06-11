aa969ab64bcba343f405c9f6c107e6457a99c9b7
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-06-11 22:21:18 +0900

    add,change:()
    add
    -ambientLight,directionalLight,pointLightの追加
    
    change
    - マテリアルをNormal => Standardに変更
    - 背景色を変更

--- app/page.tsx
+++ app/page.tsx
@@ -48,8 +48,8 @@ export default function Home() {
       });
 
       //  マテリアル
-      const textMaterial = new THREE.MeshNormalMaterial();
-      const sphereMaterial = new THREE.MeshNormalMaterial();
+      const textMaterial = new THREE.MeshStandardMaterial();
+      const sphereMaterial = new THREE.MeshStandardMaterial();
       sphereMaterial.transparent = true;
       sphereMaterial.opacity = 0.6;
 
@@ -86,9 +86,67 @@ export default function Home() {
         sphere.rotation.x = randomRotaiton.x;
         sphere.rotation.y = randomRotaiton.y;
 
-        sphere.scale.set(0.25, 0.25, 0.25);
+        sphere.scale.set(0.15, 0.15, 0.15);
         scene.add(sphere);
       }
+
+      // ライト
+      const ambientLightParams = {
+        color: 0xffffff,
+        intensity: 1,
+      };
+
+      const ambientLight = new THREE.AmbientLight(
+        ambientLightParams.color,
+        ambientLightParams.intensity
+      );
+
+      const directionalLightParams = {
+        color: 0xebcd8f,
+        intensity: 2,
+      };
+
+      const directionalLight = new THREE.DirectionalLight(
+        directionalLightParams.color,
+        directionalLightParams.intensity
+      );
+      directionalLight.position.set(0, -1, 2);
+
+      const pointLightParams = {
+        color: 0xebcd8f,
+        intensity: 6,
+        distance: 10,
+        decay: 2,
+      };
+
+      for (let i = 0; i < 10; i += 1) {
+        const pointLight = new THREE.PointLight(
+          pointLightParams.color,
+          pointLightParams.intensity,
+          pointLightParams.distance,
+          pointLightParams.decay
+        );
+        const randomPosition = {
+          x: (Math.random() - 0.5) * 10,
+          y: (Math.random() - 0.5) * 10,
+          z: (Math.random() - 0.5) * 10,
+        };
+        pointLight.position.x = randomPosition.x;
+        pointLight.position.y = randomPosition.y;
+        pointLight.position.z = randomPosition.z;
+
+        scene.add(pointLight);
+      }
+
+      scene.add(ambientLight, directionalLight);
+
+      // ヘルパー
+      const directionalLightHelper = new THREE.DirectionalLightHelper(
+        directionalLight,
+        0.2
+      );
+
+      // scene.add(directionalLightHelper);
 
       // デバッグUI
       const gui = new GUI({
@@ -143,11 +201,11 @@ export default function Home() {
       // Renderer
       const renderer = new THREE.WebGLRenderer({
         canvas: canvas,
-        alpha: true,
+        // alpha: true,
         antialias: true,
       });
       renderer.setSize(sizes.width, sizes.height);
-      renderer.setClearColor("#FFA84E");
+      renderer.setClearColor("#7DD3FC");
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
       // ループアニメーション
