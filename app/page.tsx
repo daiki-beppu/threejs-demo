010755db818dbc2a9d78be9a59e4c136a8d8a706
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-06-20 00:41:22 +0900

    add:()
    - ギターの作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,473 @@
 "use client";
 
-import gsap from "gsap";
 import GUI from "lil-gui";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { Timer } from "three/addons/misc/Timer.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-
+  const guiRef = useRef<GUI | null>(null);
   useEffect(() => {
-    if (canvasRef.current) {
+    if (canvasRef.current && !guiRef.current) {
       // Canvas
       const canvas = canvasRef.current;
 
       // Scene
       const scene = new THREE.Scene();
 
-      // テクスチャ
-
-      //  マテリアル
-      const textMaterial = new THREE.MeshStandardMaterial({ color: 0xebcd8f });
-      const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xfffffe });
-
-      // オブジェクト
-      // 3D テキスト
-      const fontLoader = new FontLoader();
-      fontLoader.load("/fonts/gentilis_regular.typeface.json", (font) => {
-        const textGeometry = new TextGeometry("welcome !", {
-          font: font,
-          size: 1,
-          depth: 0.2,
-          curveSegments: 5,
-          bevelEnabled: true,
-          bevelThickness: 0.03,
-          bevelSize: 0.02,
-          bevelOffset: 0,
-          bevelSegments: 4,
-        });
-
-        const text = new THREE.Mesh(textGeometry, textMaterial);
-        textGeometry.center();
-        text.castShadow = true;
-        scene.add(text);
+      // デバッグUI
+      const gui = new GUI({
+        width: 300,
+        title: "デバッグUI",
+        // closeFolders: true,
       });
+      // gui.hide();
+      guiRef.current = gui;
 
-      const planeParams = {
-        width: 10,
-        height: 10,
+      // ギターグループ
+      const guiterGroup = new THREE.Group();
+      guiterGroup.position.x = -3.5;
+
+      guiterGroup.rotation.z = -Math.PI / 3;
+      guiterGroup.rotation.x = -Math.PI / 9;
+      scene.add(guiterGroup);
+
+      // ギターのボディ
+      // オブジェクト
+      const bodyParams = {
+        width: 4.2,
+        height: 5.5,
+        depth: 0.28,
       };
 
-      const plane = new THREE.Mesh(
-        new THREE.PlaneGeometry(planeParams.width, planeParams.height),
-        planeMaterial
+      const body = new THREE.Mesh(
+        new THREE.BoxGeometry(
+          bodyParams.width,
+          bodyParams.height,
+          bodyParams.depth
+        ),
+        new THREE.MeshStandardMaterial({ color: "#D7A625" })
       );
 
-      plane.position.set(0, 0, -1);
-      plane.receiveShadow = true;
-      scene.add(plane);
+      guiterGroup.add(body);
+
+      const bodyFolder = gui.addFolder("ボディ");
+      bodyFolder.add(body.scale, "x").min(0.1).max(10).step(0.01).name("幅");
+      bodyFolder.add(body.scale, "y").min(0.1).max(10).step(0.01).name("高さ");
+      bodyFolder
+        .add(body.scale, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("奥行き");
+      bodyFolder
+        .add(body.position, "x")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("x軸 移動");
+      bodyFolder
+        .add(body.position, "y")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("y軸 移動");
+      bodyFolder
+        .add(body.position, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("z軸 移動");
+
+      // ギターのネック
+      const neckParams = {
+        width: 0.8,
+        height: 6.2,
+        depth: 0.15,
+      };
+
+      const neck = new THREE.Mesh(
+        new THREE.BoxGeometry(
+          neckParams.width,
+          neckParams.height,
+          neckParams.depth
+        ),
+        new THREE.MeshStandardMaterial({ color: "#734e30" })
+      );
+
+      neck.position.y = bodyParams.width;
+      neck.position.z = bodyParams.depth / 2;
+
+      guiterGroup.add(neck);
+
+      const neckFolder = gui.addFolder("ネック");
+      neckFolder.add(neck.scale, "x").min(0.1).max(10).step(0.01).name("幅");
+      neckFolder.add(neck.scale, "y").min(0.1).max(10).step(0.01).name("高さ");
+      neckFolder
+        .add(neck.scale, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("奥行き");
+      neckFolder
+        .add(neck.position, "x")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("x軸 移動");
+      neckFolder
+        .add(neck.position, "y")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("y軸 移動");
+      neckFolder
+        .add(neck.position, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("z軸 移動");
+
+      // ヘッド
+
+      const headParams = {
+        width: 1.3,
+        height: 1.5,
+        depth: 0.28,
+      };
+
+      const head = new THREE.Mesh(
+        new THREE.BoxGeometry(
+          headParams.width,
+          headParams.height,
+          headParams.depth
+        ),
+        new THREE.MeshStandardMaterial({ color: "black" })
+      );
+
+      head.position.y = bodyParams.width + neckParams.height / 2;
+      head.position.z = bodyParams.depth / 2;
+
+      guiterGroup.add(head);
+
+      const headFolder = gui.addFolder("ヘッド");
+      headFolder.add(head.scale, "x").min(0.1).max(10).step(0.01).name("幅");
+      headFolder.add(head.scale, "y").min(0.1).max(10).step(0.01).name("高さ");
+      headFolder
+        .add(head.scale, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("奥行き");
+      headFolder
+        .add(head.position, "x")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("x軸 移動");
+      headFolder
+        .add(head.position, "y")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("y軸 移動");
+      headFolder
+        .add(head.position, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("z軸 移動");
+
+      // ペグ
+
+      const pegGroup = new THREE.Group();
+
+      const pegParams = {
+        radius: 0.1,
+        segments: 16,
+      };
+
+      const peg1stString = new THREE.Mesh(
+        new THREE.CircleGeometry(pegParams.radius, pegParams.segments),
+        new THREE.MeshStandardMaterial({ color: "silver" })
+      );
+      peg1stString.position.x = headParams.width / 5;
+      peg1stString.position.y =
+        (bodyParams.height + neckParams.height + headParams.height) / 2 + 0.4;
+      peg1stString.position.z = headParams.depth + 0.01;
+      pegGroup.add(peg1stString);
+
+      // 1弦のペグ
+      const pegFolder = gui.addFolder("ペグ");
+      const peg1stStringFolder = pegFolder.addFolder("1弦ペグ");
+      peg1stStringFolder
+        .add(pegParams, "radius")
+        .min(0.01)
+        .max(10)
+        .step(0.01)
+        .name("半径")
+        .onChange((value: number) => {
+          peg1stString.geometry.dispose();
+          peg1stString.geometry = new THREE.CircleGeometry(
+            value,
+            pegParams.segments
+          );
+        });
+
+      peg1stStringFolder
+        .add(peg1stString.position, "x")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("x軸 移動");
+      peg1stStringFolder
+        .add(peg1stString.position, "y")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("y軸 移動");
+      peg1stStringFolder
+        .add(peg1stString.position, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("z軸 移動");
+
+      // 2弦のペグ
+
+      const peg2ndString = new THREE.Mesh(
+        new THREE.CircleGeometry(pegParams.radius, pegParams.segments),
+        new THREE.MeshStandardMaterial({ color: "silver" })
+      );
+      peg2ndString.position.x = headParams.width / 5 + 0.075;
+      peg2ndString.position.y =
+        (bodyParams.height + neckParams.height + headParams.height) / 2 + 0.8;
+      peg2ndString.position.z = headParams.depth + 0.01;
+
+      pegGroup.add(peg2ndString);
+
+      const peg2ndStringFolder = pegFolder.addFolder("2弦ペグ");
+      peg2ndStringFolder
+        .add(pegParams, "radius")
+        .min(0.01)
+        .max(10)
+        .step(0.01)
+        .name("半径")
+        .onChange((value: number) => {
+          peg2ndString.geometry.dispose();
+          peg2ndString.geometry = new THREE.CircleGeometry(
+            value,
+            pegParams.segments
+          );
+        });
+
+      peg2ndStringFolder
+        .add(peg2ndString.position, "x")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("x軸 移動");
+      peg2ndStringFolder
+        .add(peg2ndString.position, "y")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("y軸 移動");
+      peg2ndStringFolder
+        .add(peg2ndString.position, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("z軸 移動");
+
+      // 3弦のペグ
+      const peg3rdString = new THREE.Mesh(
+        new THREE.CircleGeometry(pegParams.radius, pegParams.segments),
+        new THREE.MeshStandardMaterial({ color: "silver" })
+      );
+      peg3rdString.position.x = headParams.width / 5 + 0.15;
+      peg3rdString.position.y =
+        (bodyParams.height + neckParams.height + headParams.height) / 2 + 1.2;
+      peg3rdString.position.z = headParams.depth + 0.01;
+
+      pegGroup.add(peg3rdString);
+
+      const peg3rdStringFolder = pegFolder.addFolder("3弦ペグ");
+      peg3rdStringFolder
+        .add(pegParams, "radius")
+        .min(0.01)
+        .max(10)
+        .step(0.01)
+        .name("半径")
+        .onChange((value: number) => {
+          peg3rdString.geometry.dispose();
+          peg3rdString.geometry = new THREE.CircleGeometry(
+            value,
+            pegParams.segments
+          );
+        });
+
+      peg3rdStringFolder
+        .add(peg3rdString.position, "x")
+        .min(0.1)
+        .max(6)
+        .step(0.01)
+        .name("x軸 移動");
+      peg3rdStringFolder
+        .add(peg3rdString.position, "y")
+        .min(0.1)
+        .max(6)
+        .step(0.01)
+        .name("y軸 移動");
+      peg3rdStringFolder
+        .add(peg3rdString.position, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("z軸 移動");
+
+      // 4弦のペグ
+      const peg4thString = new THREE.Mesh(
+        new THREE.CircleGeometry(pegParams.radius, pegParams.segments),
+        new THREE.MeshStandardMaterial({ color: "silver" })
+      );
+      peg4thString.position.x = -headParams.width / 5;
+      peg4thString.position.y =
+        (bodyParams.height + neckParams.height + headParams.height) / 2 + 0.4;
+      peg4thString.position.z = headParams.depth + 0.01;
+
+      pegGroup.add(peg4thString);
+
+      const peg4thStringFolder = pegFolder.addFolder("4弦ペグ");
+      peg4thStringFolder
+        .add(pegParams, "radius")
+        .min(0.01)
+        .max(10)
+        .step(0.01)
+        .name("半径")
+        .onChange((value: number) => {
+          peg4thString.geometry.dispose();
+          peg4thString.geometry = new THREE.CircleGeometry(
+            value,
+            pegParams.segments
+          );
+        });
+
+      peg4thStringFolder
+        .add(peg4thString.position, "x")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("x軸 移動");
+      peg4thStringFolder
+        .add(peg4thString.position, "y")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("y軸 移動");
+      peg4thStringFolder
+        .add(peg4thString.position, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("z軸 移動");
+
+      // 5弦のペグ
+
+      const peg5thString = new THREE.Mesh(
+        new THREE.CircleGeometry(pegParams.radius, pegParams.segments),
+        new THREE.MeshStandardMaterial({ color: "silver" })
+      );
+      peg5thString.position.x = -headParams.width / 5 - 0.075;
+      peg5thString.position.y =
+        (bodyParams.height + neckParams.height + headParams.height) / 2 + 0.8;
+      peg5thString.position.z = headParams.depth + 0.01;
+
+      pegGroup.add(peg5thString);
+
+      const peg5thStringFolder = pegFolder.addFolder("5弦ペグ");
+      peg5thStringFolder
+        .add(pegParams, "radius")
+        .min(0.01)
+        .max(10)
+        .step(0.01)
+        .name("半径")
+        .onChange((value: number) => {
+          peg5thString.geometry.dispose();
+          peg5thString.geometry = new THREE.CircleGeometry(
+            value,
+            pegParams.segments
+          );
+        });
+
+      peg5thStringFolder
+        .add(peg5thString.position, "x")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("x軸 移動");
+      peg5thStringFolder
+        .add(peg5thString.position, "y")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("y軸 移動");
+      peg5thStringFolder
+        .add(peg5thString.position, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("z軸 移動");
+
+      // 6弦のペグ
+      const peg6thString = new THREE.Mesh(
+        new THREE.CircleGeometry(pegParams.radius, pegParams.segments),
+        new THREE.MeshStandardMaterial({ color: "silver" })
+      );
+      peg6thString.position.x = -headParams.width / 5 - 0.15;
+      peg6thString.position.y =
+        (bodyParams.height + neckParams.height + headParams.height) / 2 + 1.2;
+      peg6thString.position.z = headParams.depth + 0.01;
+
+      pegGroup.add(peg6thString);
+
+      const peg6thStringFolder = pegFolder.addFolder("6弦ペグ");
+      peg6thStringFolder
+        .add(pegParams, "radius")
+        .min(0.01)
+        .max(10)
+        .step(0.01)
+        .name("半径")
+        .onChange((value: number) => {
+          peg6thString.geometry.dispose();
+          peg6thString.geometry = new THREE.CircleGeometry(
+            value,
+            pegParams.segments
+          );
+        });
+
+      peg6thStringFolder
+        .add(peg6thString.position, "x")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("x軸 移動");
+      peg6thStringFolder
+        .add(peg6thString.position, "y")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("y軸 移動");
+      peg6thStringFolder
+        .add(peg6thString.position, "z")
+        .min(0.1)
+        .max(10)
+        .step(0.01)
+        .name("z軸 移動");
+
+      guiterGroup.add(pegGroup);
 
       // ライト
       const ambientLightParams = {
@@ -112,14 +520,6 @@ export default function Home() {
       directionalLighCameratHelper.visible = false;
       scene.add(directionalLighCameratHelper);
 
-      // デバッグUI
-      const gui = new GUI({
-        width: 400,
-        title: "デバッグUI",
-        closeFolders: true,
-      });
-      gui.hide();
-
       // directionalLight
       const directionalLightDebugUI = gui.addFolder("directionalLight");
 
@@ -167,7 +567,7 @@ export default function Home() {
         100
       );
 
-      camera.position.z = 7;
+      camera.position.z = 10;
       scene.add(camera);
 
       // Contorols
@@ -177,11 +577,10 @@ export default function Home() {
       // Renderer
       const renderer = new THREE.WebGLRenderer({
         canvas: canvas,
-        // alpha: true,
+        alpha: true,
         antialias: true,
       });
       renderer.setSize(sizes.width, sizes.height);
-      renderer.setClearColor("#7DD3FC");
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
       // 影を有効にする
@@ -189,9 +588,16 @@ export default function Home() {
       renderer.shadowMap.type = THREE.PCFShadowMap;
 
       // ループアニメーション
+      const timer = new Timer();
+
       const animetion = () => {
         controls.update();
         renderer.render(scene, camera);
+
+        // タイマー
+        timer.update();
+        const elapsedTime = timer.getElapsed();
+
         requestAnimationFrame(animetion);
       };
       animetion();
